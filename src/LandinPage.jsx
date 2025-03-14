6a9f9e38938ae1f3544d400587e4a4cdf7c99129
AndVig <andrea.vigna2@gmail.com>
2025-03-14 13:33:17 +0100

    [UPD] Remove unused state and effect hooks from LandingPage component for cleaner code

--- src/LandinPage.jsx
+++ src/LandinPage.jsx
@@ -1,26 +1,11 @@
-import { useState, useEffect } from "react";
-import { fetchMoviesAndSeries } from "./store";
+
 import "./App.css";
 import Carousel from "./components/Carousel.jsx";
 
 function LandingPage() {
-  const [movies, setMovies] = useState([]);
-  const [series, setSeries] = useState([]);
+  
 
-  useEffect(() => {
-    async function loadItems() {
-      try {
-        const data = await fetchMoviesAndSeries();
-        const movies = data.filter((item) => item.media_type === "movie");
-        const series = data.filter((item) => item.media_type === "tv");
-        setMovies(movies);
-        setSeries(series);
-      } catch (error) {
-        console.error("Failed to load items:", error);
-      }
-    }
-    loadItems();
-  }, []);
+
 
   return (
     <div className="container w-screen">

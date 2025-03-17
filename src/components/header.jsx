f2be2e336ced6c09f58a0da8ec58161c310e872b
AndVig <andrea.vigna2@gmail.com>
2025-03-17 22:34:28 +0100

    Enhance header navigation with Watchlist link and update search container styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css';
 import reactLogo from '../assets/react.svg';
 import Search from './Search';
@@ -6,17 +7,15 @@ import Search from './Search';
 function Header() {
   return (
     <header className="bg-[#242424] fixed top-0 w-full shadow-md z-50">
-      <nav className=" mx-auto flex  items-center justify-between p-4">
-        <a href="/" className="flex items-center">
+      <nav className="container mx-auto flex items-center justify-between p-4">
+        <Link to="/" className="flex items-center">
           <img src={reactLogo} alt="Logo" className="h-8 w-8 mr-2" />
-          <span className="text-xl font-bold text-cyan-500">Home</span>
-        </a>
+          <Link to="/watchlist" className="text-xl font-bold text-cyan-500 ml-4">
+            Watchlist
+          </Link>
+          
+        </Link>
         <div className="flex items-center">
-          {/* <input
-            type="text"
-            placeholder="Search..."
-            className="px-4 py-2 border w-50 md:w-11/12 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          /> */}
           <Search />
         </div>
       </nav>

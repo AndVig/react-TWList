ff489c041c1252b39e4f29f949b6800d5140d564
AndVig <andrea.vigna2@gmail.com>
2025-03-18 22:32:09 +0100

    Adjust margin-top of landing page container for improved layout

--- src/LandinPage.jsx
+++ src/LandinPage.jsx
@@ -8,7 +8,7 @@ function LandingPage() {
 
 
   return (
-    <div className="container w-screen mt-30">
+    <div className="container w-screen mt-25">
       <Carousel type="movie" title="Movies" />
       <Carousel type="tv" title="Series" />
     </div>

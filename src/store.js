a43bc2c0abead659432add7850baff7a1602e0ce
AndVig <andrea.vigna2@gmail.com>
2025-04-01 19:07:37 +0200

    Remove console logs for API key and request URLs in store.js

--- src/store.js
+++ src/store.js
@@ -1,12 +1,10 @@
 const apiKey = import.meta.env.VITE_API_KEY; // Legge la chiave API dalla variabile di ambiente
-console.log('API Key:', apiKey); // Aggiungi questo log per verificare la chiave API
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 // Funzione per recuperare i film e le serie TV più popolari
 export async function fetchMoviesAndSeries() {
   const url = `${BASE_URL}/trending/all/week?api_key=${apiKey}`;
-  console.log('Request URL:', url); // Logga l'URL della richiesta
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error('Failed to fetch movies and series');
@@ -18,7 +16,6 @@ export async function fetchMoviesAndSeries() {
 // Funzione per recuperare i dettagli di un film o di una serie TV
 export async function fetchCredits(mediaType, id) {
   const url = `${BASE_URL}/${mediaType}/${id}/credits?api_key=${apiKey}`;
-  console.log('Request URL:', url); // Logga l'URL della richiesta
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error('Failed to fetch credits');
@@ -32,7 +29,7 @@ export async function fetchCredits(mediaType, id) {
 // Funzione per cercare film e serie TV
 export async function searchItems(query) {
   const url = `${BASE_URL}/search/multi?api_key=${apiKey}&query=${encodeURIComponent(query)}`;
-  console.log('Request URL:', url); // Logga l'URL della richiesta
+  
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error('Failed to search items');

09e6768169079fc8e11ee96af8955ef41ab6e24d
AndVig <andrea.vigna2@gmail.com>
2025-04-01 19:56:20 +0200

    Update logo source in Header component to use Vite logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,7 +9,7 @@ function Header() {
     <header className="bg-[#242424] fixed top-0 w-full shadow-md z-50">
       <nav className="container mx-auto flex items-center justify-between px-5">
         <Link to="/" className="flex items-center">
-          <img src={reactLogo} alt="Logo" className="h-8 w-8 mr-2" />
+        <img src="/vite.svg" alt="Logo" className="h-8 w-8 mr-2" />
           <Link to="/watchlist" className="text-xl font-bold text-cyan-500 ml-4">
             Watchlist
           </Link>

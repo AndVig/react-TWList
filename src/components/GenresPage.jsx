938297d9426a9563b00ca3d1d516e19e843d12f1
AndVig <andrea.vigna2@gmail.com>
2025-04-03 00:40:11 +0200

    Refactor card styling for responsiveness; update GenresPage layout and modal display for improved user experience

--- src/components/GenresPage.jsx
+++ src/components/GenresPage.jsx
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { fetchMovieGenres, fetchSeriesGenres, fetchMoviesByGenre, fetchSeriesByGenre } from '../store';
-import { Link } from 'react-router-dom';
-import Card from './Card'; // Importa il componente Card
+import React, { useState, useEffect } from "react";
+import {
+  fetchMovieGenres,
+  fetchSeriesGenres,
+  fetchMoviesByGenre,
+  fetchSeriesByGenre,
+} from "../store";
+import { Link } from "react-router-dom";
+import Card from "./Card"; // Importa il componente Card
 
 function normalizeGenreName(name) {
-  return name
-    .toLowerCase()
-    .replace(/ & /g, '-')
-    .replace(/\s+/g, '-');
+  return name.toLowerCase().replace(/ & /g, "-").replace(/\s+/g, "-");
 }
 
 function GenresPage() {
@@ -17,7 +19,7 @@ function GenresPage() {
   const [movies, setMovies] = useState([]); // Film del genere selezionato
   const [series, setSeries] = useState([]); // Serie TV del genere selezionato
   const [isModalOpen, setIsModalOpen] = useState(false); // Stato della modale
-  const [modalType, setModalType] = useState(''); // Tipo di contenuto nella modale (movies o series)
+  const [modalType, setModalType] = useState(""); // Tipo di contenuto nella modale (movies o series)
 
   useEffect(() => {
     async function loadGenres() {
@@ -28,7 +30,7 @@ function GenresPage() {
         const seriesData = await fetchSeriesGenres();
         setSeriesGenres(seriesData.genres || []);
       } catch (error) {
-        console.error('Failed to load genres:', error);
+        console.error("Failed to load genres:", error);
       }
     }
     loadGenres();
@@ -37,26 +39,34 @@ function GenresPage() {
   // Gestisce il click su un genere di film
   const handleMovieGenreClick = async (genre) => {
     setSelectedGenre(genre);
-    setModalType('movies');
+    setModalType("movies");
     try {
       const movies = await fetchMoviesByGenre(genre.id); // Recupera i film del genere selezionato
-      setMovies(movies);
+      const moviesWithMediaType = movies.map((movie) => ({
+        ...movie,
+        media_type: "movie", // Aggiungi il campo media_type
+      }));
+      setMovies(moviesWithMediaType);
       setIsModalOpen(true); // Mostra la modale
     } catch (error) {
-      console.error('Failed to fetch movies by genre:', error);
+      console.error("Failed to fetch movies by genre:", error);
     }
   };
 
   // Gestisce il click su un genere di serie TV
   const handleSeriesGenreClick = async (genre) => {
     setSelectedGenre(genre);
-    setModalType('series');
+    setModalType("series");
     try {
       const series = await fetchSeriesByGenre(genre.id); // Recupera le serie TV del genere selezionato
-      setSeries(series);
+      const seriesWithMediaType = series.map((serie) => ({
+        ...serie,
+        media_type: "tv", // Aggiungi il campo media_type
+      }));
+      setSeries(seriesWithMediaType);
       setIsModalOpen(true); // Mostra la modale
     } catch (error) {
-      console.error('Failed to fetch series by genre:', error);
+      console.error("Failed to fetch series by genre:", error);
     }
   };
 
@@ -66,7 +76,7 @@ function GenresPage() {
     setSelectedGenre(null);
     setMovies([]);
     setSeries([]);
-    setModalType('');
+    setModalType("");
   };
 
   return (
@@ -83,7 +93,9 @@ function GenresPage() {
               onClick={() => handleMovieGenreClick(genre)}
               className="w-40 h-40 md:w-80 md:h-80 bg-cover bg-center text-white flex items-center justify-center rounded-md hover:opacity-80 transition-opacity duration-300"
               style={{
-                backgroundImage: `url(/genres/${normalizeGenreName(genre.name)}.jpg)`,
+                backgroundImage: `url(/genres/${normalizeGenreName(
+                  genre.name
+                )}.jpg)`,
               }}
             >
               <span className="bg-black bg-opacity-50 px-2 py-1 rounded text-sm">
@@ -96,7 +108,9 @@ function GenresPage() {
 
       {/* Mosaico per i generi delle serie TV */}
       <div className="genres-section">
-        <h2 className="text-xl text-center font-semibold mb-3">Series Genres</h2>
+        <h2 className="text-xl text-center font-semibold mb-3">
+          Series Genres
+        </h2>
         <div className="flex justify-center flex-wrap gap-4">
           {seriesGenres.map((genre) => (
             <button
@@ -104,7 +118,9 @@ function GenresPage() {
               onClick={() => handleSeriesGenreClick(genre)}
               className="w-40 h-40 md:w-80 md:h-80 bg-cover bg-center text-white flex items-center justify-center rounded-md hover:opacity-80 transition-opacity duration-300"
               style={{
-                backgroundImage: `url(/genres/${normalizeGenreName(genre.name)}.jpg)`,
+                backgroundImage: `url(/genres/${normalizeGenreName(
+                  genre.name
+                )}.jpg)`,
               }}
             >
               <span className="bg-black bg-opacity-50 px-2 py-1 rounded text-sm">
@@ -118,34 +134,20 @@ function GenresPage() {
       {/* Modale */}
       {isModalOpen && (
         <div className="fixed inset-0 bg-gray-900 flex items-center justify-center z-50">
-          <div className="bg-blue-500 w-full h-full md:w-3/4 md:h-3/4 rounded-lg overflow-y-auto p-5 relative animate-fade-in">
+          <div className="bg-blue-500 w-full h-full md:w-3/4 md:h-3/4 rounded-lg overflow-y-auto py-5 px-2 pr-6 relative animate-fade-in">
             <button
               onClick={closeModal}
-              className="fixed top-5 right-5 text-white text-xl font-bold"
+              className="fixed z-52 top-5 right-5 text-white text-xl font-bold"
             >
               &times;
             </button>
-            <h2 className="w-4/5 text-2xl font-bold mb-5">
-              Most Popular {modalType === 'movies' ? 'Movies' : 'Series'} in {selectedGenre?.name}
+            <h2 className="w-4/5 text-2xl px-5 font-bold mb-5">
+              Most Popular {modalType === "movies" ? "Movies" : "Series"} in{" "}
+              {selectedGenre?.name}
             </h2>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {(modalType === 'movies' ? movies : series).map((item) => (
-                <div
-                  key={item.id}
-                  className="bg-gray-200 rounded-lg overflow-hidden shadow-md"
-                >
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                    alt={item.title || item.name}
-                    className="w-full h-60 object-cover"
-                  />
-                  <div className="p-2">
-                    <h3 className="text-lg font-semibold">{item.title || item.name}</h3>
-                    <p className="text-sm text-gray-600">
-                      Rating: {item.vote_average}
-                    </p>
-                  </div>
-                </div>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+              {(modalType === "movies" ? movies : series).map((item) => (
+                <Card key={item.id} item={item} />
               ))}
             </div>
           </div>
